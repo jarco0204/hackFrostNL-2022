5c4f6e9e92bf8f926cb34367a0af3824d1a3c131
Johan Arcos <jarco0204@outlook.com>
2022-02-19 15:34:35 -0330

    front-end finished

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,11 +1,44 @@
+import './main.css';
+
 // Another way to define a function component
 const CalculatorPage  = () => {
-    let APIURL = "http://localhost:8000";
+    // let APIURL = "http://localhost:8000";
 
    
-    // }, []);
-    return (<div>
-        <h1>Hellow World, I will be connecting to {APIURL}</h1>
+    return (<div className='center'>
+        <h1>Welcome to Nuclear Calculator!</h1>
+        <div className='inputs'>
+            <div>
+                <h3>Number 1:</h3>
+                <input type="number" name='number1'/>
+            </div>
+            <div>
+                <h3>Number 2: </h3>
+                <input type="number" name='number2'/>
+            </div> 
+        </div>
+        <div className='operation'>
+            <div>
+                <input type="radio" value="add" name="operation" /> Add
+            </div>
+            <div>
+                <input type="radio" value="subtraction" name="operation" /> Subtraction
+            </div>
+            <div>
+                <input type="radio" value="multiplication" name="operation" /> Multiplication
+            </div>
+            <div>   
+                <input type="radio" value="dvision" name="operation" /> Division
+            </div>
+        </div>
+        <div className="buttons">
+            <div>
+                <button id="button1" type="button">POST Inputs to Server</button>
+            </div>
+            <div>
+                <button id="button2" type="button">GET Output</button>
+            </div>
+        </div>
     </div>);
 };
 
