67a8f28bd66e3b3d4a5cfaf8dbb816a8621c13eb
Johan Arcos <jarco0204@outlook.com>
2022-02-19 16:04:11 -0330

    adding hooks to manager state: front-end

--- README.md
+++ README.md
@@ -18,3 +18,4 @@ git checkout 'commitHash'
 3. POST: f69739489605a366b0ecadfb7adcd06a88ec92c6
 4. ModifiedGET & Backend Done 6c166d19c7892a17b23b26eb6faa3ad7c0f7380d
 5. Front-end React: 5ee93dae171e141ce861db6bd484aef7d621af5b
+6. Front-end Done: 5c4f6e9e92bf8f926cb34367a0af3824d1a3c131
--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,23 +1,52 @@
+import { useState } from "react";
+
 import './main.css';
 
 // Another way to define a function component
 const CalculatorPage  = () => {
+
+    //Hooks
+    const [number1Input, setNumber1Input] = useState(0);
+    const [number2Input, setNumber2Input] = useState(0);
+    const [operationInput, setOperationInput] = useState("");
+    const [answer, setAnswer] = useState("");
+
+
+
     // let APIURL = "http://localhost:8000";
 
-   
+    // HANDLERS
+    const sendData = () =>{
+        console.log(number1Input,number2Input, operationInput)
+    }
+
+    const handleInputChange = (event) => {
+        if (event.target.id === "number1") {
+            setNumber1Input(event.target.value);
+        } else if (event.target.id === "number2") {
+            setNumber2Input(event.target.value);
+        }
+        
+    };
+    const handleOperationChange = (event) =>{
+        setOperationInput(event.target.value)
+        // console.log(event.target.value)
+    }
+
+   // Actual Component
     return (<div className='center'>
         <h1>Welcome to Nuclear Calculator!</h1>
         <div className='inputs'>
             <div>
                 <h3>Number 1:</h3>
-                <input type="number" name='number1'/>
+                <input type="number" id="number1" name='number1' value={number1Input} onChange={handleInputChange}/>
             </div>
             <div>
                 <h3>Number 2: </h3>
-                <input type="number" name='number2'/>
+                <input type="number" id="number2" name='number2' value={number2Input} onChange={handleInputChange}/>
             </div> 
         </div>
-        <div className='operation'>
+        <div className='operation' onChange={handleOperationChange}>
             <div>
                 <input type="radio" value="add" name="operation" /> Add
             </div>
@@ -25,21 +54,26 @@ const CalculatorPage  = () => {
                 <input type="radio" value="subtraction" name="operation" /> Subtraction
             </div>
             <div>
-                <input type="radio" value="multiplication" name="operation" /> Multiplication
+                <input type="radio" value="multiplication" name="operation"   /> Multiplication
             </div>
             <div>   
-                <input type="radio" value="dvision" name="operation" /> Division
+                <input type="radio" value="division" name="operation"  /> Division
             </div>
         </div>
         <div className="buttons">
             <div>
-                <button id="button1" type="button">POST Inputs to Server</button>
+                <button id="button1" type="button" onClick={sendData}>POST Inputs to Server</button>
             </div>
             <div>
                 <button id="button2" type="button">GET Output</button>
             </div>
         </div>
-    </div>);
+        <h3>Result:</h3>
+        <p>{answer}</p>
+
+    </div>
+    
+    );
 };
 
 export default CalculatorPage;
